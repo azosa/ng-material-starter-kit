21f7a7d17c1e4f7e966b4241bd6391dd4fb184bc
Paulina <azosa93@gmail,com>
2022-12-11 01:36:37 +0100

    filtered list of products based on subject added

--- src/app/components/filtered-products-list/filtered-products-list.component.ts
+++ src/app/components/filtered-products-list/filtered-products-list.component.ts
@@ -15,14 +15,27 @@ import { ProductsService } from '../../services/products.service';
 export class FilteredProductsListComponent {
   readonly categories$: Observable<string[]> = this._categoriesService.getAll();
 
-
-
+  private _categorySubject: Subject<string> = new Subject<string>();
+  public category$: Observable<string> = this._categorySubject.asObservable();
   readonly products$: Observable<ProductModel[]> = combineLatest([
- this._productsService.getAll(),
-  this._activatedRoute.params
-  ]).pipe(map(([products,params]:[ProductModel[],Params])=>{
-    return products.filter((products:ProductModel)=>products.category===params['category']);
-   }));
+    this._productsService.getAll(),
+    this.category$
+  ]).pipe(
+    map(([products, category]: [ProductModel[], string]) => {
+      return products.filter(product => product.category === category)
+    })
+  );
+
+  selectCategory(category:string): void {
+    this._categorySubject.next(category);
+  }
+
+ //  readonly products$: Observable<ProductModel[]> = combineLatest([
+ // this._productsService.getAll(),
+ //  this._activatedRoute.params
+ //  ]).pipe(map(([products,params]:[ProductModel[],Params])=>{
+ //    return products.filter((products:ProductModel)=>products.category===params['category']);
+ //   }));
 
 
   constructor(private _categoriesService: CategoriesService, private _productsService: ProductsService, private _activatedRoute: ActivatedRoute) {
